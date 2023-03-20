08349357d77008bec363ee40bc49d0c0f91c0197
Ryan Earl <ryanleoearl@gmail.com>
2023-03-20 01:53:49 -0700

    Added minimal js, html, and css files

---
+++ index.js
@@ -0,0 +1,47 @@
+directories = [
+  '',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7'
+]
+
+var getDirectories = directories;
+
+function createHead() {
+  const head = document.getElementById("head");
+  var title = document.createElement("Title");
+  title.innerHTML = "Smart Dog Collar"
+  head.appendChild(title);
+}
+
+function createHeader() {
+  const header = document.getElementById("header");
+  var h1 = document.createElement("h1");
+  var gary = document.createElement("a");
+  var nav = document.createElement("nav");
+  gary.innerHTML = "Smart Dog Collar";
+  gary.setAttribute("href", "/");
+  h1.appendChild(gary);
+  nav.setAttribute("id", "nav")
+  for (i = 0; i<= getDirectories.length-1; i++) {
+    var list = document.createElement("li");
+    var item = document.createElement("a");
+    item.setAttribute("class", getDirectories[i]);
+    if (getDirectories[i] == "") {
+      getDirectories[i] = "Home";
+      item.setAttribute("href", "/");
+    }
+    else {
+      item.setAttribute("href", "./pages/"+getDirectories[i]+".html");
+    }
+    item.innerHTML = getDirectories[i];
+    list.appendChild(item)
+    nav.appendChild(list);
+  }
+  header.appendChild(h1);
+  header.appendChild(nav);
+}
